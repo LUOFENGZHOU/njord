286d06b5070cea2ac01f1720f1c4c5aabb00c520
mdhoffschmidt <mdhoffschmidt@protonmail.com>
2018-10-22 23:32:21 +0100

    history udpate

--- test/test_history.py
+++ test/test_history.py
@@ -11,8 +11,18 @@ from njordtoolbox import History
 
 if __name__ == "__main__":
 
-	# Set an empty dataframe.
-	df = pandas.DataFrame()
+	# Create an empty dataframe.
+	df = []
+	for i in range(10):
+		data = {
+			"time": datetime.datetime.now(),
+			"price": random.normalvariate(100.0, 1.0)
+		}
+		df.append(data)
+	df = pandas.DataFrame(df).set_index("time")
+
+	# Display the dataframe.
+	print(df)
 
 	# Set the record.
 	record = History(df, name="random")
