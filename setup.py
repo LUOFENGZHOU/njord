f7d3b508a12b5629cc74f70e4391b3281c9e3267
mdhoffschmidt <mdhoffschmidt@protonmail.com>
2018-12-03 13:45:43 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,13 @@
 
 from setuptools import setup
 
-
 setup(name="njord",
 		version="0.2",
-		description="toolbox for the njord project",
-		url="https://github.com/mdhoffschmidt/njord",
+		description="njord project: package for datasience and cryptotrading",
+		url="https://github.com/njord-project/njord",
 		author="mdhoffschmidt",
 		author_email="mdhoffschmidt@protonmail.com",
 		license=None,
 		packages=["njord"],
-		install_requires=["numpy", "pandas", "torch"],
+		install_requires=["numpy", "pandas"],
 		zip_safe=False)
